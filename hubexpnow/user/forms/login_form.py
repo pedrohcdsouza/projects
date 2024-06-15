24173f239dd5c9fa733582cf50b2b1d77e9c467b
pedrohcdsouza <pedrohcsousa8@hotmail.com>
2024-06-14 23:06:42 -0300

    Adding login system

---
+++ hubexpnow/user/forms/login_form.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label = 'Usuário',
+        widget=forms.TextInput(attrs={'placeholder': 'Digite seu usuário'})
+    )
+
+    password = forms.CharField(
+        label = 'Senha',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Digite sua senha'})
+    )
--- hubexpnow/user/views.py
+++ hubexpnow/user/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 def register_get(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -29,7 +31,27 @@ def register_post(request):
     return redirect('user:register')
 
 def login_get(request):
-    return render(request, 'user/pages/login.html')
+    form = LoginForm()
+    return render(request, 'user/pages/login.html', {
+        'form': form,
+        'form_action': reverse('user:login_post')
+    })
 
 def login_post(request):
-    return render(request, 'user/pages/login.html')
+    if not request.POST:
+        raise Http404
+    
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username',''),
+            password=form.cleaned_data.get('password',''),
+        )
+        if authenticated_user is not None:
+            messages.success(request, 'Logado com sucesso.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Usuário e/ou senha inválidas.')
+    else:
+        messages.error(request, 'Erro ao validar formulário.')
+    return redirect(reverse('user:login'))
