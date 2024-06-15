742d0fd63f3d9dcfd96547cb42225610041d1327
pedrohcdsouza <pedrohcsousa8@hotmail.com>
2024-06-14 22:19:31 -0300

    Adding requirements.txt

--- hubexpnow/user/forms.py
+++ hubexpnow/user/forms.py
@@ -37,6 +37,31 @@ class RegisterForm(forms.ModelForm):
             
         }
 def clean_first_name(self):
-    data = self.cleaned_data.get('first_name')
-    
-    return data
+    first_name = self.cleaned_data.get('first_name', '')
+    exists_first_name = User.objects.filter(first_name=first_name).exists()
+
+    if exists_first_name:
+        raise ValidationError(
+            'A razão social utilizada já está cadastrado no BD.', code='invalid'
+        )
+    return first_name
+
+def clean_username(self):
+    username = self.cleaned_data.get('username', '')
+    exists_username = User.objects.filter(username=username).exists()
+
+    if exists_username:
+        raise ValidationError(
+            'O usuário utilizado já está cadastrado no BD.', code='invalid'
+        )
+    return username
+
+def clean_email(self):
+    email = self.cleaned_data.get('email', '')
+    exists = User.objects.filter(email=email).exists()
+
+    if exists:
+        raise ValidationError(
+            'O e-mail utilizado já está cadastrado no BD.', code='invalid'
+        )
+    return email
